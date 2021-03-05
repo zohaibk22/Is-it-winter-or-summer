faf61153b97a1f1e60df8e6c1f197a89d4f8bcd4
Zohaib <zohaibk1111@gmail.com>
2021-03-04 22:32:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+// import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "./Components/SeasonDisplay/SeasonDisplay";
+
+import SeasonDisplay from "./Components/SeasonDisplay/SeasonDisplay";
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO THIS.STATE...NO WHERE ELSE EVER
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
+
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we called setState to update state
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (error) => {
+        this.setState({
+          errorMessage: error.message,
+        });
+      }
+    );
+  }
+
+  // componentDidUpdate() {
+  //   console.log("My component was just updated- it rerendered");
+  // }
+
+  render() {
+    if (this.state.lat) {
+      return (
+        <>
+          {/* <div className="App">Latitude: {this.state.lat} </div> */}
+          <SeasonDisplay latitude={this.state.lat} />
+        </>
+      );
+    } else if (this.state.errorMessage) {
+      return (
+        <>
+          <div className="App">Error: {this.state.errorMessage} </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="App">Loading </div>
+        </>
+      );
+    }
+  }
 }
-
 export default App;
